5a66356c989216a7def024c7a9aeb102ba08c933
EridanusTL <1184498133@qq.com>
2024-01-22 00:52:04 +0800

    Added opencv library

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -37,3 +38,31 @@ plt.ylabel("dP")
 
 
 plt.show()
+=======
+import g4f
+
+g4f.debug.logging = True  # Enable logging
+g4f.check_version = False  # Disable automatic version checking
+print(g4f.version)  # Check version
+print(g4f.Provider.Ails.params)  # Supported args
+
+# Automatic selection of provider
+
+# Streamed completion
+# response = g4f.ChatCompletion.create(
+#     model="gpt-3.5-turbo",
+#     messages=[{"role": "user", "content": "Hello"}],
+#     stream=True,
+# )
+
+# for message in response:
+#     print(message, flush=True, end="")
+
+# Normal response
+response = g4f.ChatCompletion.create(
+    model=g4f.models.gpt_4,
+    messages=[{"role": "user", "content": "你好"}],
+)  # Alternative model setting
+
+print(response)
+>>>>>>> 872288d (Added opencv library)
