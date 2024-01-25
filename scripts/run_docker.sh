cf2e83c0c5f15548c233d84f1d03e9787a6386ad
timothyyu <timothyyu@tencent.com>
2024-01-25 18:03:38 +0800

    Modified run_docker.sh

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
-docker run --rm \
-    --gpus \
-    --workdir $(pwd)
+
+docker_image=tlbot/env:latest
+$(docker pull $docker_image)
+
+id=tlbot_latest
+# Attaches to `id` container if it already started.
+if [ ! -z "$(docker ps --filter name=$id | grep $id)" ]; then
+    echo "Attach to existing Docker container [$id]"
+    docker exec --interactive --tty $id /bin/bash
+    exit 0  
+fi
+
+docker run \
+    --name=$id\
+    --rm \
+    --interactive \
+    --tty \
+    --gpus all\
+    --workdir $(pwd)\
+    --volume "/run/user:/run/user" \
+    --volume "/tmp:/tmp" \
+    --volume "/dev:/dev" \
+    --volume "$HOME/.ssh:$HOME/.ssh" \
+    --volume "/etc/localtime:/etc/localtime:ro" \
+    --volume "/etc/passwd:/etc/passwd:ro" \
+    --volume "/etc/shadow:/etc/shadow:ro" \
+    --volume "/etc/group:/etc/group:ro" \
+    --volume "/etc/gshadow:/etc/gshadow:ro" \
+    --volume "/etc/apt/apt.conf:/etc/apt/apt.conf:ro" \
+    --volume "$HOME/.cache:$HOME/.cache:rw" \
+    --volume "$pwd/scripts/bashrc:$HOME/.bashrc:ro" \
+    --volume "$HOME/.ccache:$HOME/.ccache:rw" \
+    --tmpfs "$HOME:exec,rw,uid=$(id -u)" \
+    --tmpfs "$HOME/.vscode-server:exec,rw,uid=$(id -u)" \
+    --volume $pwd:$pwd \
+    --user $(id -u) \
+    $docker_image
+    bash --rcfile ~/.bashrc
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-#include <Eigen/Dense>
+#include <vector>
 
-int main(int argc, char **argv)
+class LowPassFilter
 {
-    std::cout << acos(-1) << std::endl;
+private:
+    double alpha;
+    double lastOutput;
+
+public:
+    LowPassFilter(double alpha) : alpha(alpha), lastOutput(0) {}
+
+    double process(double input)
+    {
+        double output = alpha * input + (1 - alpha) * lastOutput;
+        lastOutput = output;
+        return output;
+    }
+};
+
+int main()
+{
+    // 示例：使用0.1作为alpha值
+    LowPassFilter lpf(0.1);
+
+    // 假设的输入数据
+    std::vector<double> inputData = {1.0, 2.0, 3.0, 4.0, 5.0, 4.0, 3.0, 2.0, 1.0};
+
+    std::cout << "Filtered data: ";
+    for (double data : inputData)
+    {
+        std::cout << lpf.process(data) << " ";
+    }
 
     return 0;
 }
