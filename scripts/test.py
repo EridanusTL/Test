91ae9eedb173c4aba37a6b352e1b2f5f199564b2
EridanusTL <1184498133@qq.com>
2024-06-04 01:22:24 +0800

    Added bazel-based compilation

--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,27 @@
+import cv2
 import numpy as np
 
-while True:
-    degree = input("num:")
-    degree = float(degree)
-    kg = 3.3134978149967033457988961965629 * degree
-    print("kg = ", kg)
-5
+# 图片路径
+img = cv2.imread("dataset/2.jpg")
+a = []
+b = []
+
+
+def on_EVENT_LBUTTONDOWN(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        xy = "%d,%d" % (x, y)
+        a.append(x)
+        b.append(y)
+        cv2.circle(img, (x, y), 1, (0, 0, 255), thickness=-1)
+        cv2.putText(
+            img, xy, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), thickness=1
+        )
+        cv2.imshow("image", img)
+        print(x, y)
+
+
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", on_EVENT_LBUTTONDOWN)
+cv2.imshow("image", img)
+cv2.waitKey(0)
+print(a[0], b[0])
