e8fd51c9fadd660c70e4454e70697604b769f8f3
EridanusTL <1184498133@qq.com>
2024-07-21 02:19:22 +0800

    Added glog base on bazel compalition

---
+++ WORKSPACE
@@ -0,0 +1,15 @@
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+    name = "com_github_gflags_gflags",
+    sha256 = "34af2f15cf7367513b352bdcd2493ab14ce43692d2dcd9dfc499492966c64dcf",
+    strip_prefix = "gflags-2.2.2",
+    urls = ["https://github.com/gflags/gflags/archive/v2.2.2.tar.gz"],
+)
+
+http_archive(
+    name = "com_github_google_glog",
+    sha256 = "c17d85c03ad9630006ef32c7be7c65656aba2e7e2fbfc82226b7e680c771fc88",
+    strip_prefix = "glog-0.7.1",
+    urls = ["https://github.com/google/glog/archive/v0.7.1.zip"],
+)
--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,20 @@
+#include <glog/logging.h>
 #include <iostream>
 
-int main(int argc, char **argv)
-{
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  FLAGS_logtostderr = 1;
+  FLAGS_colorlogtostderr = 1;
+
+  //   FLAGS_alsologtostderr = 1;
+  //   google::SetLogSeverityLevel(google::GLOG_INFO);
+
+  std::cout << "Hello World!" << std::endl;
+
+  LOG(INFO) << "This is an info message";
+  LOG(WARNING) << "This is a warning message";
+  LOG(ERROR) << "This is an error message";
+
+  //   google::ShutdownGoogleLogging();
+  return 0;
 }
