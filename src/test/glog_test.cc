59867ade38812e5ccf30f475f5013de902ce9a59
TLBOT <1184498133@qq.com>
2025-01-06 11:27:51 +0800

    Modified Eigen and glog library

--- src/test/glog_test.cc
+++ src/test/glog_test.cc
@@ -1,16 +1,18 @@
+#ifndef GLOG_USE_GLOG_EXPORT
+#  define GLOG_USE_GLOG_EXPORT
+#endif
+
 #include <glog/logging.h>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-  google::InitGoogleLogging(argv[0]);
+  google::InitGoogleLogging("glog_test");
   FLAGS_logtostderr = 1;
   FLAGS_colorlogtostderr = 1;
 
-  //   FLAGS_alsologtostderr = 1;
-  //   google::SetLogSeverityLevel(google::GLOG_INFO);
-
   std::cout << "Hello World!" << std::endl;
 
+  LOG(INFO) << argv[0];
   LOG(INFO) << "This is an info message";
   LOG(WARNING) << "This is a warning message";
   LOG(ERROR) << "This is an error message";
