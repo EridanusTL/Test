e4b2d0f6e1c0515ea03308aecf895dacfd5fda79
TLBOT <1184498133@qq.com>
2025-01-15 11:48:53 +0800

    Added key dection code

--- src/test/thread_test.cc
+++ src/test/thread_test.cc
@@ -5,6 +5,7 @@
 
 std::mutex mtx1;
 std::mutex mtx2;
+std::timed_mutex time_mtx;
 
 class A {
  public:
@@ -24,14 +25,25 @@ class A {
   }
 };
 
-int main(int argc, char* argv[]) {
-  std::shared_ptr<A> a = std::make_shared<A>();
+void fun(int& x) {
+  for (size_t i = 0; i < 2; i++) {
+    std::unique_lock<std::timed_mutex> lock(time_mtx, std::defer_lock);
+    if (lock.try_lock_for(std::chrono::seconds(1))) {
+      std::this_thread::sleep_for(std::chrono::seconds(2));
+      x++;
+    }
+  }
+}
 
-  std::thread t1(&A::fun1, a);
-  std::thread t2(&A::fun2, a);
+int main(int argc, char* argv[]) {
+  int x = 0;
+
+  std::thread t1(&fun, std::ref(x));
+  std::thread t2(&fun, std::ref(x));
 
   t1.join();
   t2.join();
+  std::cout << "x: " << x << std::endl;
   std::cout << "Thread joined!" << std::endl;
 
   return 0;
