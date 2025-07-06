de17a12e02b5117be51c15df7ae8489e293d5050
TLBOT <1184498133@qq.com>
2025-07-06 20:24:43 +0800

    Add torch_nn_test.py

---
+++ python/test/torch_nn_test.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ModelTest(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(10, 5)
+
+    def forward(self, input):
+        output = input + 1
+        return output
+        # return self.linear(input)
+
+
+if __name__ == "__main__":
+    input = torch.tensor(
+        [
+            [1, 2, 0, 3, 1],
+            [0, 1, 2, 3, 1],
+            [1, 2, 1, 0, 0],
+            [5, 2, 3, 1, 1],
+            [2, 1, 0, 1, 1],
+        ]
+    )
+    kernel = torch.tensor([[1, 2, 1], [0, 1, 0], [2, 1, 0]])
+    input = torch.reshape(input, (1, 1, 5, 5))
+    kernel = torch.reshape(kernel, (1, 1, 3, 3))
+    output = F.conv2d(input, kernel)
+
+    print(input.shape)
+    print(input)
+    print(output)
